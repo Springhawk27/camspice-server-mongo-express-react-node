758a53f5ccf636afebe40cc7da6b6a0c3b5c7173
Springhawk27 <sajjad0mahmud@gmail.com>
2021-11-13 20:40:45 +0600

    almost done

--- index.js
+++ index.js
@@ -2,32 +2,56 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+const admin = require("firebase-admin");
+
 
 const cors = require('cors');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
+const serviceAccount = require('./camspice-3c20d-firebase-adminsdk-bl7i8-4676e4c069.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42wwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42wwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
+
 
 async function run() {
     try {
 
         await client.connect();
-        // console.log('connected to database');
 
         // database creation if theres none
         const database = client.db('CamSpice');
         // collection or simply a table
         const productCollection = database.collection('products');
         const accessoriesCollection = database.collection('accessories');
-        // const bookingSpotCollection = database.collection('bookings');
+        const orderCollection = database.collection('orders');
+        const userCollection = database.collection('users');
+
 
 
         // GET API -  all products
@@ -37,11 +61,11 @@ async function run() {
             res.send(products);
         });
 
-        // GET API -  all bookings
-        app.get('/bookings', async (req, res) => {
-            const cursor = bookingSpotCollection.find({});
-            const bookings = await cursor.toArray();
-            res.send(bookings);
+        // GET API -  all orders
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
         });
         // GET API -  accessories
         app.get('/accessories', async (req, res) => {
@@ -52,26 +76,26 @@ async function run() {
 
 
 
-        // GET API - single spot
-        app.get('/picnicSpots/:id', async (req, res) => {
+        // GET API - single product
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const picnicSpot = await picnicSpotCollection.findOne(query);
-            res.json(picnicSpot)
+            const product = await productCollection.findOne(query);
+            res.json(product)
         })
 
-        // add bookings api
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body;
+        // add orders api
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
             // console.log('my bookings', bookings);
-            const result = await bookingSpotCollection.insertOne(booking);
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
 
         // put or update  api
 
-        app.put('/bookings/:id', async (req, res) => {
+        app.put('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const options = { upsert: true }
@@ -80,10 +104,7 @@ async function run() {
                     status: "Approved"
                 },
             };
-            const result = await bookingSpotCollection.updateOne(filter, updateDoc, options)
-            // console.log('updating user', id);
-            // console.log('updating user req', req);
-            // res.send('updating not dating');
+            const result = await orderCollection.updateOne(filter, updateDoc, options)
             res.json(result);
         })
 
@@ -115,20 +136,89 @@ async function run() {
         });
 
         // DELETE API
-        app.delete('/bookings/:id', async (req, res) => {
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await bookingSpotCollection.deleteOne(query);
+            const result = await orderCollection.deleteOne(query);
             res.json(result);
 
         })
-        app.delete('/picnicSpots/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await picnicSpotCollection.deleteOne(query);
+
+
+        app.get('/users', async (req, res) => {
+
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.json(users);
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(user);
+            console.log(result);
+            res.json(result)
+        });
+
+
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
             res.json(result);
 
+        });
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+
+            const requester = req.decodedEmail;
+
+            if (requester) {
+                const requesterAccount = await userCollection.findOne({ email: requester })
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = {
+                        $set: { role: 'admin' }
+                    };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+
+            }
+            else {
+                res.status(401).json({ message: 'You are not authorized' })
+            }
+
         })
+
+
+
+        // app.delete('/products/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await productCollection.deleteOne(query);
+        //     res.json(result);
+
+        // })
+
+
     }
     finally {
         // await client.close();
